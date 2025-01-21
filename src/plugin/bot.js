2233d1535172644a7aba2a5e9519e1f0464c8635
Malvin King <malvink003@gmail.com>
2025-01-21 10:02:47 +0200

    Add files via upload

---
+++ src/plugin/bot.js
@@ -0,0 +1,24 @@
+import config from '../../config.cjs';
+
+const ping = async (m, sock) => {
+  const prefix = config.PREFIX;
+const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
+const text = m.body.slice(prefix.length + cmd.length).trim();
+
+  if (cmd === "bot") {
+    const start = new Date().getTime();
+    await m.React('⚡');
+    const end = new Date().getTime();
+    const responseTime = (end - start) / 1000;
+
+    const text = `*Nexus x𝚖𝚍 𝚋𝚘𝚝 𝚎𝚗𝚊𝚋𝚕𝚎d
+    
+   𝚔𝚎𝚎𝚙 𝚌𝚊𝚕𝚖 𝚍𝚞𝚍𝚎
+╭──────────────────
+┋https://whatsapp.com/channel/0029Vade9VgD38CPEnxfYF0M
+╰┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈`;
+    sock.sendMessage(m.from, { text }, { quoted: m });
+  }
+}
+
+export default ping;
